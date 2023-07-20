aedb3bef7b0f0fc2de3501db81f487c5c90227b7
Florian Spieß <business@minnced.club>
2023-07-20 13:08:12 +0200

    Update RequestError

--- twitch-api/src/error.rs
+++ twitch-api/src/error.rs
@@ -1,62 +1,34 @@
-use std::{
-    error::Error,
-    fmt::{Display, Formatter},
-};
-
 use reqwest::{header::ToStrError, StatusCode};
+use thiserror::Error;
 
-pub type AsyncError = Box<dyn Error + Send + Sync>;
-pub type SerdeError = serde_json::Error;
-
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum RequestError {
+    #[error("http request failed with code {0}")]
     Http(StatusCode),
+    #[error("request timed out")]
     Timeout,
-    Unexpected(AsyncError),
-    Deserialize(SerdeError),
+    #[error("unexpected error: {0:?}")]
+    Unexpected(#[from] anyhow::Error),
+    #[error("failed to deserialize")]
+    Deserialize(#[from] serde_json::Error),
+    #[error("{0} not found for query {1}")]
     NotFound(&'static str, String),
 }
 
-impl Display for RequestError {
-    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
-        match self {
-            RequestError::Http(status) => write!(f, "HTTP {}", status),
-            RequestError::Unexpected(e) => write!(f, "Unexpected error: {}", e),
-            RequestError::Deserialize(e) => write!(f, "Deserialize error: {}", e),
-            RequestError::Timeout => write!(f, "Timeout error"),
-            RequestError::NotFound(kind, query) => write!(f, "{kind} not found for query {query}"),
-        }
-    }
-}
-
-impl std::error::Error for RequestError {}
-
 impl From<reqwest::Error> for RequestError {
     fn from(e: reqwest::Error) -> Self {
-        RequestError::Unexpected(Box::new(e))
-    }
-}
-
-impl From<AsyncError> for RequestError {
-    fn from(e: AsyncError) -> Self {
-        RequestError::Unexpected(e)
+        RequestError::Unexpected(e.into())
     }
 }
 
 impl From<ToStrError> for RequestError {
     fn from(e: ToStrError) -> Self {
-        RequestError::Unexpected(Box::new(e))
+        RequestError::Unexpected(e.into())
     }
 }
 
 impl From<StatusCode> for RequestError {
     fn from(code: StatusCode) -> Self {
         RequestError::Http(code)
-    }
-}
-
-impl From<SerdeError> for RequestError {
-    fn from(e: SerdeError) -> Self {
-        RequestError::Deserialize(e)
     }
 }
